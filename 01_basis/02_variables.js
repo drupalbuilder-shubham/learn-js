c78316a011b7debea3c3bf0a29eb3840d8b6a21a
drupalbuilder-shubham <133983331+drupalbuilder-shubham@users.noreply.github.com>
2024-03-15 09:34:44 +0000

    dat types

---
+++ 01_basis/02_variables.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert(3+3);
+
+console.log(3+3)
+ console.log('Shubham')
+
+ //number => 2 to power of 53
+ //bigint
+ //boolean =>true/false
+ //null =>standerd value
+ //symbol=> unique
+ //undefined=>
+ //String=>""
